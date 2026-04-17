e2f985920c47237abf6cdc43465cad5e1ee5cb7a
Fernando <100nandoo@gmail.com>
2026-04-17 13:44:51 +0800

    chore: initial project setup with Jetpack Compose Android app

---
+++ app/src/main/java/dev/halim/knobdroid/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package dev.halim.knobdroid.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val primaryLight = Color(0xFF82542F)
+val secondaryLight = Color(0xFF725948)
+val tertiaryLight = Color(0xFF5D622E)
+
+val primaryDark = Color(0xFFF6BA8D)
+val secondaryDark = Color(0xFFE0C0AA)
+val tertiaryDark = Color(0xFFC6CB8C)
