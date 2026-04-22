369ed5de9fcbb7e74052e0ef31672901eb71edab
Fernando <100nandoo@gmail.com>
2026-04-22 11:41:43 +0800

    change color to match icon color

--- app/src/main/java/dev/halim/knobdroid/ui/theme/Color.kt
+++ app/src/main/java/dev/halim/knobdroid/ui/theme/Color.kt
@@ -2,10 +2,11 @@ package dev.halim.knobdroid.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val primaryLight = Color(0xFF82542F)
-val secondaryLight = Color(0xFF725948)
-val tertiaryLight = Color(0xFF5D622E)
+// Derived from launcher icon accent (#3DDC84) and dark background (#212121)
+val primaryLight = Color(0xFF1B6740)
+val secondaryLight = Color(0xFF4E6357)
+val tertiaryLight = Color(0xFF366B52)
 
-val primaryDark = Color(0xFFF6BA8D)
-val secondaryDark = Color(0xFFE0C0AA)
-val tertiaryDark = Color(0xFFC6CB8C)
+val primaryDark = Color(0xFF3DDC84)
+val secondaryDark = Color(0xFFA8C5B5)
+val tertiaryDark = Color(0xFF66E8A8)
